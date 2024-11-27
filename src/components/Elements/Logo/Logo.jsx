c954b2fe415d3c0dafdd8132d70e2f9e4ff28b82
Muhammad Rafif Ramadhansyah <ramadansyah.rafif@gmail.com>
2024-11-27 17:37:42 +0700

    change img source for app logo

--- src/components/Elements/Logo/Logo.jsx
+++ src/components/Elements/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div className="flex shrink-0 items-center">
-      <img alt="SkyTicket Logo" src="/src/assets/logo.svg" className="w-auto" />
+      <img alt="SkyTicket Logo" src="/src/assets/logo1.svg" className="w-auto" />
     </div>
   );
 };
