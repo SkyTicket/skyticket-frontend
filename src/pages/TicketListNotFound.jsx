7ae86e800d020896966e22a78cccefff7d7ee1e0
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2024-11-28 22:40:38 +0700

    add TicketListNotFound

---
+++ src/pages/TicketListNotFound.jsx
@@ -0,0 +1,11 @@
+import TicketListPage from "./TicketListPage";
+
+const TicketListPageNotFound = () => {
+  return (
+    <div className="">
+      <TicketListPage image="/src/assets/icons/notfound.svg" />
+    </div>
+  );
+};
+
+export default TicketListPageNotFound;
