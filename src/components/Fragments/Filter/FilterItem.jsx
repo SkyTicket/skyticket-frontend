cdf8b432e49e01e33d5f104d0eeb933f5ca4b5f6
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2024-11-28 22:37:28 +0700

    modify FilterItem

--- src/components/Fragments/Filter/FilterItem.jsx
+++ src/components/Fragments/Filter/FilterItem.jsx
@@ -1,24 +1,26 @@
 const FilterItem = ({ icon, label, hasDropdown = true }) => {
   return (
     <div className=" flex items-center justify-between p-4 hover:bg-gray-50 cursor-pointer">
-      <div className="flex items-center gap-3">
+      <div className=" flex items-center gap-3">
         {icon}
         <span>{label}</span>
       </div>
       {hasDropdown && (
-        <svg
-          className="w-4 h-4"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M9 5l7 7-7 7"
-          />
-        </svg>
+        <div className="text-slate-500 hover:text-black">
+          <svg
+            className="w-4 h-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </div>
       )}
     </div>
   );
