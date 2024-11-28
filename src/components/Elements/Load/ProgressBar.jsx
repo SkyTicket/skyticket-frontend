3c7d2d2cca2f696892649ae3ca60f951ac4f4633
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2024-11-28 22:32:39 +0700

    modify ProgressBar

--- src/components/Elements/Load/ProgressBar.jsx
+++ src/components/Elements/Load/ProgressBar.jsx
@@ -3,18 +3,19 @@ import "../../../../public/css/loaderAnimation.css";
 
 const ProgressBar = () => {
   return (
-    <div className="relative h-16 w-72 bg-gray-200 rounded-lg overflow-hidden">
-      {/* Progress Bar */}
-      <div className="absolute top-0 left-0 h-full w-full bg-purple-700 animate-loader"></div>
-      {/* Man pushing */}
-      <div className="absolute h-full w-16 flex justify-center items-center animate-push">
-        <img
-          src="/src/assets/icons/man.svg"
-          alt="man pushing"
-          className="h-15"
-        />
-      </div>
-    </div>
+    <div className="relative flex  border-2 border-black h-16 w-72 bg-gray-200 rounded-lg overflow-hidden">
+        {/* Progress Bar */}
+        <div className="absolute top-0 left-0 h-full w-full bg-purple-700 animate-loader z-10"></div>
+        
+        {/* Man pushing */}
+        <div className="absolute z-20 -left-16 h-full flex items-center animate-push">
+          <img
+            src="/src/assets/icons/man.svg"
+            alt="man pushing"
+            className="h-14"
+          />
+        </div>
+      </div>
   );
 };
 
