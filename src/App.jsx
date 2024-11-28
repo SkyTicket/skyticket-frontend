fbb56b9e90ccf4931301370157e64c80d1f1d3ab
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2024-11-28 22:42:01 +0700

    modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TicketListPage from "./pages/TicketListPage";
-
+import TicketListPageNotFound from "./pages/TicketListNotFound";
 function App() {
   return (
     <Router>
-      <TicketListPage/>
+      <Routes>
+        <Route path="/" element={<TicketListPage />} />
+        <Route path="*" element={<TicketListPageNotFound />} />
+      </Routes>
     </Router>
   );
 }
