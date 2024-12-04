51add6b468f4aa6bb98041be11b92248d36b6c32
Muhammad Rafif Ramadhansyah <ramadansyah.rafif@gmail.com>
2024-12-04 22:16:29 +0700

    refactor code structure

--- src/components/Elements/Button/Input.jsx
+++ src/components/Elements/Button/Input.jsx
@@ -1,7 +1,7 @@
 const Input = ({ className = "", ...props }) => {
   return (
     <input
-      className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600 ${className}`}
+      className={`w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600 ${className}`}
       {...props}
     />
   );
--- src/components/Elements/Date/DateList.jsx
+++ src/components/Elements/Date/DateList.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import Date from "../../Fragments/Date/Date";
 const DateList = () => {
-
   return (
     <div className="border-b-4">
       <div className="flex overflow-x-auto pb-4">
