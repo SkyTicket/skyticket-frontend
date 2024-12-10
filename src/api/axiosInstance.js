0684f311e2cc6cede643595b5800127661b3ce1e
Muhammad Rafif Ramadhansyah <ramadansyah.rafif@gmail.com>
2024-12-10 12:34:50 +0700

    change and put base url inside .env

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
   },
