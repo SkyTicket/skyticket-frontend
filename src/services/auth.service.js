6d048ce6480ce35a244322d98fe6901fac3466ea
Muhammad Rafif Ramadhansyah <ramadansyah.rafif@gmail.com>
2024-12-15 22:29:29 +0700

    modify register service, add verify and resend OTP services

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -50,22 +50,17 @@ const Register = async (data) => {
   try {
     const response = await axiosInstance.post("/api/v1/auth/register", data);
 
-    if (response.data.token) {
-      Cookies.set("token", response.data.token, { expires: 7 });
-      return {
-        status: "Success",
-        message: response.data.message,
-        token: response.data.token,
-      };
-    } else {
-      return {
-        status: "Error",
-        message: response.data.message,
-      };
+    if (response.status === 201) {
+      Cookies.set("userEmail", data.user_email, { expires: 7 });
     }
+
+    return {
+      status: "Success",
+      message: response.data.message,
+      email: data.user_email,
+    };
   } catch (error) {
-    const errorMessage =
-      error.response?.data?.message || "Terjadi kesalahan saat registrasi";
+    const errorMessage = error.response?.data?.message;
     return {
       status: "Error",
       message: errorMessage,
@@ -73,4 +68,31 @@ const Register = async (data) => {
   }
 };
 
-export { Login, Logout, Register };
+const VerifyOtp = async (email, otpCode) => {
+  try {
+    const response = await axiosInstance.post("/api/v1/auth/verify-otp", {
+      user_email: email,
+      otp_code: otpCode,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error dari VerifyOtp:", error.response?.data);
+    throw error.response?.data || new Error("Gagal verifikasi OTP");
+  }
+};
+
+const ResendOtp = async (email) => {
+  try {
+    const response = await axiosInstance.post("/api/v1/auth/resend-otp", {
+      user_email: email,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error dari ResendOtp:", error.response?.data);
+    throw error.response?.data || new Error("Gagal mengirim ulang OTP");
+  }
+};
+
+export { Login, Logout, Register, VerifyOtp, ResendOtp };
